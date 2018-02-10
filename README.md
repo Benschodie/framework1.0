85eafd4624ecc4e68f0c90edc90d29cb604351c2
Benjamin <benjamin.konopka@icloud.com>
2018-02-10 17:24:31 +0100

    adding some gulp tasks

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# framework1.0
+# Basic Develop Structure
+
+This is a Small Boilerplate for HTML 5. It includes Gulp Tasks for sass and
+Browser Sync. Its a small little Standart (just for me).
+
+####TODOS for the Future:
+
+A Grid 
+
+####How to Use
+
+To start the tasks, write 'gulp' in the terminal.
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-    console.log('worked');
+
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function() {
+    return gulp
+      .src('./app/assets/scss/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('./app/assets/css'))
+      .pipe(browserSync.stream());
 });
+
+// Watch Sass & Server
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({ 
+        server: "./app" 
+    });
+
+    gulp.watch(['./app/assets/scss/main.scss'], ['sass']);
+    gulp.watch("./app/*.html").on("change", browserSync.reload);
+});
+
+gulp.task('default', ['serve', 'sass']);
